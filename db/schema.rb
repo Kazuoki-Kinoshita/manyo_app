90546913933f90235417356279f2e48da434a6a2
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-01 18:19:16 +0900

    task_name & detailカラムのDBの制約を変更

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,10 @@
-ActiveRecord::Schema.define(version: 2023_03_01_084810) do
+ActiveRecord::Schema.define(version: 2023_03_01_091230) do
 
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task_name"
-    t.text "detail"
+    t.string "task_name", null: false
+    t.text "detail", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
