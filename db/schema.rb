7ddd17b1d25adb16e49eef4c785e27a9c6c1c626
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-01 21:57:04 +0900

    tasksテーブルのカラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,10 @@
-ActiveRecord::Schema.define(version: 2023_03_01_091230) do
+ActiveRecord::Schema.define(version: 2023_03_01_123500) do
 
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task_name", null: false
-    t.text "detail", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
