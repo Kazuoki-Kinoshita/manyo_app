0591fcc9fb5f7d94c02ce65d2fbf7eb0543d357f
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-01 22:57:16 +0900

    タスクの新規作成画面作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,5 +7,24 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:notice] = "タスク作成しました！"
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end  
 end
