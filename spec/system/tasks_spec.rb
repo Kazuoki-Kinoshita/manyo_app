77ba1dbf90531de918c33071cfa5893b74158952
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-03 20:15:52 +0900

    タスク一覧が作成日時の降順に並んでいるかのテスト追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-    let!(:task1) {FactoryBot.create(:task)}
-    let!(:task2) {FactoryBot.create(:second_task)}
+    let!(:task1) { FactoryBot.create(:task) }
+    let!(:task2) { FactoryBot.create(:second_task) }
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -17,29 +17,38 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
   
   describe '一覧表示機能' do
+    before do
+      visit tasks_path
+    end
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        visit tasks_path
         expect(page).to have_content 'task1-1'
         expect(page).to have_content 'task1-2'
         expect(page).to have_content 'task2-1'
         expect(page).to have_content 'task2-2'
       end
     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        new_task = all('.task_row tr')[0]
+        expect(new_task).to have_content 'task2-1'
+        expect(new_task).to have_content 'task2-2'
+      end
+    end
   end
 
   describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当タスクの内容が表示される' do
         visit task_path(task1)
         expect(page).to have_content 'task1-1'
         expect(page).to have_content 'task1-2'
-       end
-       it '該当タスクの内容が表示される_2' do
+      end
+      it '該当タスクの内容が表示される_2' do
         visit task_path(task2)
         expect(page).to have_content 'task2-1'
         expect(page).to have_content 'task2-2'
-       end
-     end
+      end
+    end
   end
 end
