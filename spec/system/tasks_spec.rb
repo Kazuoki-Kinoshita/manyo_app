767a59a443525fb0af6f2043e39c2e16377678a5
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-03 12:58:06 +0900

    テストコードのリファクタリング

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  before do
-    @task1 = FactoryBot.create(:task)
-    @task2 = FactoryBot.create(:second_task)
-  end
+    let!(:task1) {FactoryBot.create(:task)}
+    let!(:task2) {FactoryBot.create(:second_task)}
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -12,7 +10,6 @@ RSpec.describe 'タスク管理機能', type: :system do
         fill_in 'タイトル', with: 'new_task'
         fill_in '内容', with: 'new_content'
         click_button '登録する'
-        visit tasks_path
         expect(page).to have_content 'new_task'
         expect(page).to have_content 'new_content'
       end
@@ -22,9 +19,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
         visit tasks_path
-        expect(page).to have_content 'task'
         expect(page).to have_content 'task1-1'
         expect(page).to have_content 'task1-2'
         expect(page).to have_content 'task2-1'
@@ -36,12 +31,12 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-        visit task_path(id: @task1.id)
+        visit task_path(task1)
         expect(page).to have_content 'task1-1'
         expect(page).to have_content 'task1-2'
        end
        it '該当タスクの内容が表示される_2' do
-        visit task_path(id: @task2.id)
+        visit task_path(task2)
         expect(page).to have_content 'task2-1'
         expect(page).to have_content 'task2-2'
        end
