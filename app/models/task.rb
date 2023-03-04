8a3a050b4d12251d69d17ddc74c9301c9dac928b
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-04 16:02:42 +0900

    終了期限とステータスのバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 300 }
+  validates :expired_at, presence: true
+  validates :status, presence: true
 end
