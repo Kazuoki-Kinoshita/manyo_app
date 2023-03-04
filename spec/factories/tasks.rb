4af9a83a9c404174cee5e03c7ca8a70aa7d7e7ae
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-04 15:04:37 +0900

    終了期限をクリック時のテスト実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,10 +2,12 @@ FactoryBot.define do
   factory :task do
     title { 'task1-1' }
     content { 'task1-2' }
+    expired_at { '2023-04-25' }
   end
 
   factory :second_task, class: Task do
     title { 'task2-1' }
     content { 'task2-2' }
+    expired_at { '2023-01-01' }
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -9,9 +9,11 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'タイトル', with: 'new_task'
         fill_in '内容', with: 'new_content'
+        fill_in '終了期限', with: Date.new(2023, 5, 4)
         click_button '登録する'
         expect(page).to have_content 'new_task'
         expect(page).to have_content 'new_content'
+        expect(page).to have_content '2023-05-04'
       end
     end
   end
@@ -26,6 +28,8 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task1-2'
         expect(page).to have_content 'task2-1'
         expect(page).to have_content 'task2-2'
+        expect(page).to have_content '2023-04-25'
+        expect(page).to have_content '2023-01-01'
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
@@ -33,6 +37,16 @@ RSpec.describe 'タスク管理機能', type: :system do
         new_task = all('.task_row tr')[0]
         expect(new_task).to have_content 'task2-1'
         expect(new_task).to have_content 'task2-2'
+        expect(new_task).to have_content '2023-01-01'
+      end
+    end
+
+    context '終了期限をクリックするとタスクが降順に並んでいる場合' do
+      it '終了期限が一番遠いタスクが一番上に表示される' do
+        click_on '終了期限'
+        sleep(1)
+        most_recent_task = all('.task_row tr')[0]
+        expect(most_recent_task).to have_content '2023-04-25'
       end
     end
   end
@@ -43,11 +57,13 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit task_path(task1)
         expect(page).to have_content 'task1-1'
         expect(page).to have_content 'task1-2'
+        expect(page).to have_content '2023-04-25'
       end
       it '該当タスクの内容が表示される_2' do
         visit task_path(task2)
         expect(page).to have_content 'task2-1'
         expect(page).to have_content 'task2-2'
+        expect(page).to have_content '2023-01-01'
       end
     end
   end
