8953c4479a7676344cda81b62ac9967debc0cff5
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-06 10:10:08 +0900

    自分が作成したタスクのみ表示する機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.created_at_sorted.page(params[:page])
+    @tasks = current_user.tasks.created_at_sorted.page(params[:page])
     if params[:sort_expired]
       @tasks = Task.expired_at_sorted.page(params[:page])
     elsif params[:sort_priority]
