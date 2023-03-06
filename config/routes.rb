ff0ed9d154b82bb9af99ea4ca53e2b3953c5a7d4
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-06 11:53:40 +0900

    管理画面のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root to: 'tasks#index'
   resources :tasks
   resources :users, only: [:new, :create, :show]
+  namespace :admin do
+    resources :users
+  end
   resources :sessions, only: [:new, :create, :destroy]
 end
