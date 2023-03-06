4628f984f0fc77526e5546852da3d257676a638f
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-06 09:55:38 +0900

    ユーザ登録から同時にログインする機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(users_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザ登録しました！"
       redirect_to user_path(@user.id)
     else
