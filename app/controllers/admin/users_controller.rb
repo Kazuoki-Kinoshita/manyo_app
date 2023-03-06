93ede26b26199741c2875d4d67bdd4f5e41c796d
kinoshita kazuoki <kino05040826@gmail.com>
2023-03-06 12:13:15 +0900

    管理画面のユーザ一覧画面の表示項目変更

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,6 @@ class Admin::UsersController < ApplicationController
   skip_before_action :login_required
 
   def index
-    @users = User.all
-
-  end
+    @users = User.all.order(created_at: :desc)
+  end  
 end
